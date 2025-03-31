cdbe70da4c793c707d4e11b9477d13671600f082
Manish Kushwaha <manishkushwaha4669@gmail.com>
2025-03-31 16:19:49 +0530

    some change

--- src/app/profile/[slug]/page.jsx
+++ src/app/profile/[slug]/page.jsx
@@ -4,6 +4,8 @@ import { useRouter } from 'next/navigation';
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux';
 import { LucideUser } from 'lucide-react';
+
+
 export default function ProfilePage() {
   const user = useSelector((state) => state?.user)
   const [loading, setLoading] = useState(false);
@@ -29,10 +31,12 @@ export default function ProfilePage() {
       <section >
         <div className='m-3 flex items-center justify-evenly flex-col sm:flex-row gap-9'>
           <div className='flex flex-col gap-3'>
-            <div className='h-24 w-24 flex justify-center items-center bg-blue-100 rounded-full border-2 animate-pulse '>
-              <LucideUser size={80} />
-              {/* <img
-                src="" alt="" /> */}
+            <div className='h-24 w-24 flex justify-center items-center bg-blue-100 rounded-full border-2 '>
+              {
+                user?.avatar
+                  ? <img src={user?.ava} alt="User Profile" />
+                  : <LucideUser size={80} />
+              }
             </div>
             <div className='flex  flex-col items-center justify-center'>
               <Button> Edit</Button>
@@ -40,31 +44,31 @@ export default function ProfilePage() {
           </div>
           <div className=' flex text-left sm:text-left'>
             <p className='font-bold  text-left sm:text-left text-2xl'>
-              profile Name
+              {user?.name}
             </p>
           </div>
         </div>
         <hr />
         <div className='m-8 mt-3 flex flex-col font-semibold '>
-        <div className='flex flex-col gap-3 sm:ml-56'>
-          <p>My Order</p>
-          <p>My Order History</p>
-          <p>Save address</p>
-          <p>policy and licence </p>
-          <p>Help Center</p>
-          <p></p>
+          <div className='flex flex-col gap-3 sm:ml-56'>
+            <p>My Order</p>
+            <p>My Order History</p>
+            <p>Save address</p>
+            <p>policy and licence </p>
+            <p>Help Center</p>
+            <p></p>
 
+          </div>
+          <hr />
+          <div className='mt-2 mb-2 sm:ml-60'>
+            <button>want to sell</button>
+          </div>
+          <hr />
+          <div className='mt-2 sm:ml-60'>
+            <button> Logout</button>
+          </div>
         </div>
-        <hr />
-        <div className='mt-2 mb-2 sm:ml-60'>
-          <button>want to sell</button>
-        </div>
-        <hr />
-        <div className='mt-2 sm:ml-60'>
-          <button> Logout</button>
-        </div>
-        </div>
-       
+
       </section>
     )
   }
