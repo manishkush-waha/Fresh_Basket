94cb81f696a872fe0e3a495fda1ba8aff9a02afd
Manish Kushwaha <manishkushwaha4669@gmail.com>
2025-03-31 15:13:37 +0530

    Refactor forgot password functionality: remove old API implementation, update GET request to handle email as a URL parameter, and improve error handling in the forgot password component.

--- src/components/forgotPassword.jsx
+++ src/components/forgotPassword.jsx
@@ -18,24 +18,21 @@ const ForgotPassword = () => {
     };
 
     async function GetCode() {
-        console.log(email);
-        
         if (!email) {
             toast.error('Please enter your email address');
             return;
         }
-        axios.get('/api/forgot-password/', email)
-        .then((response)=>{
-            console.log(response.data);
-        }).catch((error)=>{
-            toast.error(error.message);
-        })
+        axios.get(`/api/forgot-password/${email}/`)
+            .then((response) => {
+                console.log(response.data);
+            }).catch((error) => {
+                toast.error(error.message);
+            })
     }
 
     async function varifyOtp() {
-        console.log(otp);
-        
-        axios.post('/api/forgot-password/', )
+
+        axios.post('/api/forgot-password/',)
     }
 
     return (
@@ -52,7 +49,7 @@ const ForgotPassword = () => {
                     <form className='flex flex-col gap-1 w-full' action={GetCode}>
                         <label htmlFor="email" className='font-bold'>Enter your email</label>
                         <div className='flex gap-1'>
-                            <input type="email" placeholder='email' id='email' className="w-full px-2 h-8 border-[1px] rounded-md border-[#a8a8a8] flex items-center justify-center" onChange={(e)=>{setEmail(e.target.value)}}/>
+                            <input type="email" placeholder='email' id='email' className="w-full px-2 h-8 border-[1px] rounded-md border-[#a8a8a8] flex items-center justify-center" onChange={(e) => { setEmail(e.target.value) }} />
                             <button className='font-bold bg-blue-600 rounded-md text-nowrap text-white px-2 cursor-pointer'>Get code</button>
                         </div>
 
@@ -72,7 +69,7 @@ const ForgotPassword = () => {
                         ))}
                     </div>
 
-                    <button disabled className="px-2 bg-blue-600 text-xl font-bold text-white py-1 rounded-xl disabled:cursor-no-drop" onClick={varifyOtp}>Verify OTP</button>
+                    <button disabled className="px-2 bg-blue-600 text-xl font-bold text-white py-1 rounded-xl disabled:cursor-no-drop disabled:bg-gray-500" onClick={varifyOtp}>Verify OTP</button>
 
                     <div className="flex gap-2">
                         <p>Didn't receive the code?</p>
