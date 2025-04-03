7b75010ea49d6db1c06345faa25ce6a9206a46b8
Manish Kushwaha <manishkushwaha4669@gmail.com>
2025-04-03 11:55:24 +0530

    Refactor RootLayout to include ChakraProvider for UI component styling

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,23 +1,25 @@
 "use client"
 import "./globals.css";
 import { ToastContainer } from 'react-toastify';
-import { Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 import store from "@/store/store";
 import MainPage from "./_main";
-
+import { ChakraProvider } from '@chakra-ui/react'
 
 export default function RootLayout({ children }) {
-  
-    return (
-      <html lang="en">
-        <body className={`work-sans-goog antialiased relative font-sans `}>
+
+  return (
+    <html lang="en">
+      <body className={`work-sans-goog antialiased relative font-sans `}>
+        <ChakraProvider >
           <Provider store={store}>
             <ToastContainer />
-            <MainPage children={children}/>
+            <MainPage children={children} />
           </Provider>
-        </body>
-      </html>
-    );
+        </ChakraProvider>
+      </body>
+    </html>
+  );
 }
 
 
